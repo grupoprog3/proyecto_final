70ba749c88371c1e6e87dc5696a0bbc60405ab8b
azfomu <azfomu@gmail.com>
2016-12-14 19:41:55 -0500

    Control de exepciones  Azael y Axel
    Controlamos que no rompa el programa por valores invalidos de evaluacion

--- proyecto/run.py
+++ proyecto/run.py
@@ -14,17 +14,23 @@ la clase Request define objetos que encapsulan toda la información relativa a u
 La función urlopen la usamos para crear nuestros pseudo-archivos. Posterior le damos lectura al objeto f,
 con el metodo f.read(), y descargamos - ordenamos en json_string en busca de las variables que nos interesa mostrar.
 """
-while 1:
-    zip = input('Para que codigo postal le gustaria ver el tiempo? ')
-    url = 'http://api.wunderground.com/api/' + key + '/geolookup/conditions/q/' + zip + '.json'
-    f = urllib.request.urlopen(url) 
-    json_string = f.read().decode('utf-8')
-    parsed_json = json.loads(json_string)
-    city = parsed_json['location']['city']
-    state = parsed_json['location']['state']
-    weather = parsed_json['current_observation']['weather']
-    temperature_string = parsed_json['current_observation']['temperature_string']
-    feelslike_string = parsed_json['current_observation']['feelslike_string']
-   
-    print ("Tiempo en %s, %s: %s. \nLa temp. es %s, pero se siente como %s." %(city,state,weather.lower(),temperature_string,feelslike_string))
-    f.close()
+control=1
+while control==1:
+    zip = input('Salir = 0, Para que codigo postal le gustaria ver el tiempo? ')
+    if zip == "0":
+        control = 0
+    else:
+        try:
+            url = 'http://api.wunderground.com/api/' + key + '/geolookup/conditions/q/' + zip + '.json'
+            f = urllib.request.urlopen(url)
+            json_string = f.read().decode('utf-8')
+            parsed_json = json.loads(json_string)
+            city = parsed_json['location']['city']
+            state = parsed_json['location']['state']
+            weather = parsed_json['current_observation']['weather']
+            temperature_string = parsed_json['current_observation']['temperature_string']
+            feelslike_string = parsed_json['current_observation']['feelslike_string']
+            print ("Tiempo en %s, %s: %s. \nLa temp. es %s, pero se siente como %s." %(city,state,weather.lower(),temperature_string,feelslike_string))
+            f.close()
+        except:
+            print("Valor Invalido, introduzca zipcode numerico")
